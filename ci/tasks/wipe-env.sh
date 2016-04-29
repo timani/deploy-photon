3ab9e0f50f92f28bfe6d229c5e789f3e7cee34d4
virtmerlin <merlin@sense.net>
2016-04-29 15:50:23 -0500

    update modified wipe-env.sh

--- ci/tasks/wipe-env.sh
+++ ci/tasks/wipe-env.sh
@@ -4,35 +4,36 @@ if [ $1 == "wipe" ];
         then
                 echo "Wiping Environment...."
         else
-		echo "Need Args [0]=wipe "
-		echo "Example: ./p1-0-wipe-env.sh wipe ..."
+                echo "Need Args [0]=wipe "
+                echo "Example: ./p1-0-wipe-env.sh wipe ..."
                 exit 1
 fi
 
 ESX_USER=$esx_user
 ESX_PASSWD=$esx_passwd
 
+IFS=',' read -r -a ESX_HOSTS <<< "$esx_hosts"
+
+
 #Grab Host IP Addresses & PowerOff/Unregister VMS
-	for x in $esx_hosts; do
-		echo "Removing VMs From host=$x ..."
-		for VM in $(vmware-cmd -U $ESX_USER -P $ESX_PASSWD --server $x -l | grep -v "vRLI"); do
-			if [ $VM != "No virtual machine found." ]; then
-				vmware-cmd -U $ESX_USER -P $ESX_PASSWD --server $x $VM stop hard
-				vmware-cmd -U $ESX_USER -P $ESX_PASSWD --server $x -s unregister $VM
-			fi
-		done
-		HOST=$x
-	done
+        for x in "${ESX_HOSTS[@]}"; do
+                echo "Removing VMs From host=$x ..."
+                for VM in $(vmware-cmd -U $ESX_USER -P $ESX_PASSWD --server $x -l | grep -v "vRLI"); do
+                        if [ $VM != "No virtual machine found." ]; then
+                                vmware-cmd -U $ESX_USER -P $ESX_PASSWD --server $x $VM stop hard
+                                vmware-cmd -U $ESX_USER -P $ESX_PASSWD --server $x -s unregister $VM
+                        fi
+                done
+                HOST=$x
+        done
 
 #Clean DataStore(s)
-	for d in $(cat deploy-photon/manifests/photon.yml | grep DATASTORE | awk -F ":" '{print $2}' |  tr "," "\n" | sort -u); do
-		vifs --server $HOST --username $ESX_USER --password $ESX_PASSWD --rm  "[$d] disks" --force || echo "Already Wiped"
-		vifs --server $HOST --username $ESX_USER --password $ESX_PASSWD --rm  "[$d] tmp_images" --force || echo "Already Wiped"
-		vifs --server $HOST --username $ESX_USER --password $ESX_PASSWD --rm  "[$d] tmp_uploads" --force || echo "Already Wiped"
-		vifs --server $HOST --username $ESX_USER --password $ESX_PASSWD --rm  "[$d] images" --force || echo "Already Wiped"
-		vifs --server $HOST --username $ESX_USER --password $ESX_PASSWD --rm  "[$d] vms" --force || echo "Already Wiped"
-		vifs --server $HOST --username $ESX_USER --password $ESX_PASSWD --rm  "[$d] vibs" --force || echo "Already Wiped"
-		vifs --server $HOST --username $ESX_USER --password $ESX_PASSWD --rm  "[$d] photon-installer" --force || echo "Already Wiped"
-	        vifs --server $HOST --username $ESX_USER --password $ESX_PASSWD --rm  "[$d] photon-cli" --force || echo "Already Wiped"
-	        vifs --server $HOST --username $ESX_USER --password $ESX_PASSWD --rm  "[$d] deleted_images" --force || echo "Already Wiped"
-	done
+        for d in $(cat deploy-photon/manifests/photon.yml | grep DATASTORE | awk -F ":" '{print $2}' |  tr "," "\n" | sort -u); do
+                vifs --server $HOST --username $ESX_USER --password $ESX_PASSWD --rm  "[$d] disks" --force || echo "Already Wiped"
+                vifs --server $HOST --username $ESX_USER --password $ESX_PASSWD --rm  "[$d] tmp_images" --force || echo "Already Wiped"
+                vifs --server $HOST --username $ESX_USER --password $ESX_PASSWD --rm  "[$d] tmp_uploads" --force || echo "Already Wiped"
+                vifs --server $HOST --username $ESX_USER --password $ESX_PASSWD --rm  "[$d] images" --force || echo "Already Wiped"
+                vifs --server $HOST --username $ESX_USER --password $ESX_PASSWD --rm  "[$d] vms" --force || echo "Already Wiped"
+                vifs --server $HOST --username $ESX_USER --password $ESX_PASSWD --rm  "[$d] vibs" --force || echo "Already Wiped"
+                vifs --server $HOST --username $ESX_USER --password $ESX_PASSWD --rm  "[$d] deleted_images" --force || echo "Already Wiped"
+        done
