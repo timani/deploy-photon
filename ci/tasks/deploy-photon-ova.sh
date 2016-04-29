745bc3f24196340755d61a6fa1b209eec0b5c385
virtmerlin <merlin@sense.net>
2016-04-29 17:04:20 -0500

    add Phase1-Task1 Deploy INstaller OVA

---
+++ ci/tasks/deploy-photon-ova.sh
@@ -0,0 +1,24 @@
+#!/bin/bash +x
+
+
+#Get Latest Photon Installer
+echo "Downloading PHOTON Installer ova..."
+wget $(wget -q -O- https://github.com/vmware/photon-controller/wiki/download | grep "install-vm.ova" | egrep -o http.*\" | tr -d "\"") -O /tmp/installer-vm.ova
+
+ovftool --acceptAllEulas --noSSLVerify --skipManifestCheck \
+--X:injectOvfEnv --overwrite --powerOffTarget --powerOn \
+--diskMode=thin \
+--net:"NAT"=${ova_network} \
+--datastore=${ova_datastore} \
+--name=photon-installer \
+--prop:ip0=${ova_ip} \
+--prop:netmask0=${ova_netmask} \
+--prop:gateway=${ova_gateway} \
+--prop:DNS=${ova_dns} \
+--prop:ntp_servers=${ova_ntp} \
+--prop:enable_syslog="true" \
+--prop:syslog_endpoint=${ova_syslog} \
+--prop:admin_password=${ova_passwd} /tmp/installer-vm.ova \
+vi://${ova_esx_user}:${ova_esx_passwd}@${ova_esx_host}
+echo "sleeping 3 minutes while CLI ova Powers On"
+sleep 180
