f0fa89caec7e260ff5862d3cfff238ec0e752019
virtmerlin <merlin@sense.net>
2016-05-09 12:23:42 -0700

    FIXED: fiel path in deploy-pcf.sh

--- ci/tasks/deploy-pcf.sh
+++ ci/tasks/deploy-pcf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-if [ ! -f manifests/pcf/${pcf_manifest} ]; then
+if [ ! -f deploy-photon/manifests/pcf/${pcf_manifest} ]; then
   echo "ERROR, Can't find ERT Manifest ${pcf_manifest}"
   exit 1
 fi
@@ -9,7 +9,7 @@ fi
 bosh -n target https://${bosh_deployment_network_ip}
 bosh -n login ${bosh_deployment_user} ${bosh_deployment_passwd}
 
-cp manifests/pcf/${pcf_manifest} /tmp/${pcf_manifest}
+cp deploy-photon/manifests/pcf/${pcf_manifest} /tmp/${pcf_manifest}
 
 perl -pi -e "s/ignore/`bosh status --uuid`/g" /tmp/${pcf_manifest}
 
