3b7b151bf2d9ad6fbb8717f437cb8acca045be57
yjyum <yjiayi@vmware.com>
2016-05-23 00:56:28 -0700

    deploy-pcf.sh: replace hardcoded network_id in cf.yml with value read from "photon network list" [Finishes #120259553]

--- ci/tasks/deploy-pcf.sh
+++ ci/tasks/deploy-pcf.sh
@@ -6,12 +6,24 @@ if [ ! -f deploy-photon/manifests/pcf/${pcf_manifest} ]; then
   exit 1
 fi
 
+# Download Photon Installer cli
+my_dir="$(dirname "$0")"
+"$my_dir/download-cli.sh"
+
 bosh -n target https://${bosh_deployment_network_ip}
 bosh -n login ${bosh_deployment_user} ${bosh_deployment_passwd}
 
 cp deploy-photon/manifests/pcf/${pcf_manifest} /tmp/${pcf_manifest}
 
+# Get Photon Network ID
+photon target set http://${ova_ip}:9000
+PHOTON_CTRL_ID=$(photon deployment list | head -3 | tail -1)
+PHOTON_CTRL_IP=$(photon deployment show $PHOTON_CTRL_ID | grep -E "LoadBalancer.*28080" | awk -F " " '{print$2}')
+photon target set http://${PHOTON_CTRL_IP}:9000
+BOSH_DEPLOYMENT_NETWORK_ID=$(photon network list | grep "$bosh_deployment_network" | awk -F " " '{print$1}')
+
 perl -pi -e "s/ignore/`bosh status --uuid`/g" /tmp/${pcf_manifest}
+perl -pi -e "s/replace_network_id/$BOSH_DEPLOYMENT_NETWORK_ID/g" /tmp/${pcf_manifest}
 
 bosh deployment /tmp/${pcf_manifest}
 bosh -n deploy
